dc2aa2065cd3f1f912ed9073897478e6a6c6d07f
Raymond Liu <wfliu66@qq.com>
2020-09-18 07:03:48 +0100

    change while condition

--- password.py
+++ password.py
@@ -1,7 +1,6 @@
 password = 'a123456'
-count = 0
 chance = 3
-while count < 3:
+while chance > 0:
     user_input = input('Please enter the password: ')
     print('password verificaiton........')
     if user_input == password:
@@ -9,7 +8,6 @@ while count < 3:
         print('Your access is granted.')
         break
     else:
-        count = count + 1
         chance = chance - 1
         print('Password verification failed.')
         print('Your access is denie')
